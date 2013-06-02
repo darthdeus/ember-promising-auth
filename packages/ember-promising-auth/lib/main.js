9f400a4b4bcda7f1c76ac4d6a0374ddb343aa98b
Jakub Arnold <darthdeus@gmail.com>
2013-06-02 12:31:27 +0200

    Working basic auth

--- packages/ember-promising-auth/lib/main.js
+++ packages/ember-promising-auth/lib/main.js
@@ -1,20 +1,78 @@
-Ember.PromisingAuth = Ember.Object.extend({
+Ember.PromisingAuth = Ember.Object.extend(Ember.Evented, {
 
-  signIn: function(params) {
-    this.signInMethod();
-    return Ember.RSVP.resolve();
+  signInEndPoint: "/users/sign_in",
+  signOutEndPoint: "/users/sign_out",
+  rememberTokenKey: "ember-promising-auth-remember-token",
+  authToken: null,
+  rememberToken: null,
+  userId: null,
+
+  signIn: function(data) {
+    var promise = Ember.$.ajax({
+      url: this.signInEndPoint,
+      type: "POST",
+      dataType: "json",
+      context: this,
+      data: data
+    });
+
+    promise.then(this._success, this._error);
+
+    return promise;
   },
 
-  signInMethod: Ember.K,
+  signOut: function() {
+    return this.ajax(this.signOutEndPoint, data, "DELETE");
+  },
 
-  ajax: function(url, method, data) {
-    data[this.token_name] = this.token;
+  recall: function() {
+    var promise = new Ember.RSVP.Promise(function(resolve, reject) {
+      // TODO
+    });
+
+    return promise;
+  },
+
+  user: function() {
+    var id = this.get("userId");
+    if (id) {
+      return App.User.find(this.get("userId"));
+    } else {
+      return null;
+    }
+  }.property("userId"),
+
+  signedIn: function() {
+    return false;
+  }.property(),
+
+  ajax: function(url, data, method) {
+    data = data || {};
+    data["auth_token"] = this.get("authToken");
 
     return Ember.$.ajax({
       url: url,
-      method: method,
+      type: method,
+      dataType: "json",
       data: data
     });
+  },
+
+  _success: function(response) {
+    this.setProperties({
+      signedIn: true,
+      authToken: response.auth_token,
+      rememberToken: response.remember_token,
+      userId: response.user_id
+    });
+
+    window.localStorage.setItem(this.rememberTokenKey, response.remember_token);
+
+    this.trigger("signInSuccess");
+  },
+
+  _error: function(xhr, status, response) {
+    this.trigger("signInError", arguments);
   }
 
 });
